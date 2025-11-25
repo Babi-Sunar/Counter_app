6c4988281bd09da79a84b9b84cd8f3529c461c16
Babi-Sunar <babi.sunar3737@gmail.com>
2025-11-25 19:24:21 +0545

    initial commit

---
+++ Projects/portfolio_project/index.js
@@ -0,0 +1,15 @@
+function yellow() {
+    const link = document.getElementById("style");
+    link.href = "yellow.css";
+
+}
+function blue() {
+    const link = document.getElementById("style");
+    link.href = "blue.css";
+
+}
+function green() {
+    const link = document.getElementById("style");
+    link.href = "green.css";
+
+}
